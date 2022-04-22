e39408ec4e73311e6a5bad0faca6103d896cf9a1
ismailcandanX <ismailcandan44@gmail.com>
2022-04-22 19:15:03 +0300

    Add complete Sign in functionality with Redux

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app'
+import { getStorage, ref } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAEVGa6Z4jwc16IlbnKrrSLriUkDPUYAYY',
+  authDomain: 'disneyplus-clone-6fd11.firebaseapp.com',
+  projectId: 'disneyplus-clone-6fd11',
+  storageBucket: 'disneyplus-clone-6fd11.appspot.com',
+  messagingSenderId: '690526184268',
+  appId: '1:690526184268:web:8df8562a599635c78f65e9',
+  measurementId: 'G-L5Y08J6W8Y',
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+// const storage = firebase.storage()
+
+const storage = getStorage();
+
+// Create a storage reference from our storage service
+const storageRef = ref(storage);
+
+export { auth, provider, storage }
+export default db
